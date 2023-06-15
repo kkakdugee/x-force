afa684d6756b80a14ccdbef6133e5eecf36b86fc
Christopher K Denq <cdenq@sas.upenn.edu>
2023-06-15 16:48:23 -0400

    Chris (#1)
    * add repo folders for organization + first bypass of gscholar api call
    
    * added gitignore + starting WoS script + helper func layout

---
+++ code/helper.py
@@ -0,0 +1,10 @@
+MASTER_CSV_COLUMNS = ["title",
+                      "authors",
+                      "schools",
+                      "countries",
+                      "year of publication",
+                      "abstract text",
+                      "url",
+                      "citation count"]
+
+SEARCH_QUERY = "radiation"
