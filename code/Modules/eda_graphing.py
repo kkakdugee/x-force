da1f3cc3a12d0e245770e1400a9b58da4bcb38cb
Christopher Denq <christopherdenq@gmail.com>
2023-06-30 17:17:17 -0400

    main.py, helper.py, minor changes to eda/arxiv modules

--- code/Modules/eda_graphing.py
+++ code/Modules/eda_graphing.py
@@ -1,1 +1,105 @@
-# this is where the final eda would go
+#----------------------------------------------------
+# Imports Checking
+#----------------------------------------------------
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+import feedparser
+import time
+from datetime import datetime
+import sys
+import helper
+
+#----------------------------------------------------
+# Graphing Class
+#----------------------------------------------------
+class XForce_Grapher():
+    def __init__(self) -> None:
+        self._data = None
+        self._sources = None
+        self._queries = None
+        self.load("../data/complete_db.csv")
+        return None
+
+
+    def peek_data(self):
+        return self._data.head()
+
+    def get_data(self):
+        return self._data
+    
+
+    def get_sources(self):
+        return self._sources
+    
+
+    def get_queries(self):
+        return self._queries
+
+
+    def set_data(self, path: str) -> None:
+        self.load(path)
+        return None
+
+
+    def load(self, path: str) -> None:
+        df = pd.read_csv(path)
+        sources = list(set(df["source"].values.tolist()))
+        sources.append("ALL")
+        queries = list(set(df["query"].values.tolist()))
+
+        self._data = df
+        self._sources = sources
+        self._queries = queries
+        return None
+
+
+    def graph_freq(self, query: str, source: str="ALL") -> None:
+        """
+        Graphs the publishing frequency of papers in the database.
+
+        query -> str
+            The given search query to visualize
+
+        source -> int
+            The database source to restrict visualizations on
+            arxiv: Graph only results from arxiv
+            scopus: Graph only results from scopus
+            ALL: Graphs all
+
+        Returns -> None
+            Prints out matplotlib graph of the published frequen
+        
+        Example
+            graph_freq("radiation", "arxiv")
+        """
+        if source not in self._sources:
+            print(f"{source} not found. Available options are {self._sources}, where 'ALL' analyzes from all sources.")
+            return None
+
+        df = self._data
+        if source != "ALL":
+            df = df[df["source"] == source]
+        df = df[df["query"] == query]
+        dates_extract = df["published"].apply(lambda x: x.split('T')[0])
+        dates = [datetime(int(i.split("-")[0]), int(i.split("-")[1]), int(i.split("-")[2])) for i in dates_extract]
+        
+        plt.title(f"Source: {source}, Query: {query}")
+        plt.suptitle(f"Publish Frequency within {len(dates)} Most Recent Papers")
+        plt.xlabel("Publish Dates")
+        plt.ylabel("Frequency")
+        plt.grid("True")
+        plt.xticks(rotation=45)
+        plt.hist(dates, 25, alpha=.75)
+        plt.show()
+        return None
+    
+#----------------------------------------------------
+# Module Checking
+#----------------------------------------------------
+def main():
+    return None
+
+if __name__ == "__main__":
+    main()
--- code/main.py
+++ code/main.py
@@ -12,47 +12,84 @@ import sys
 sys.path.append("Modules/")
 import helper
 import arxiv
+import eda_graphing
 
 #----------------------------------------------------
 # Main
 #----------------------------------------------------
-def main():
+def main() -> None:
+
     print("Welcome!")
+    current_session = eda_graphing.XForce_Grapher()
 
     while True:
-        print("Menu")
-        print("-----")
-        print("1. Update database by search term")
-        print("2. Visualizations by search term")
-        print("3. Summary of database")
-        print("4. Clean dupes from database")
-        print("5. Exit program")
-        print("0. Wipe database")
-        user_input = input("Pick your option (1-5, 0).")
-        if user_input not in {"0","1","2","3","4","5"}:
-            print("Invalid answer. Please input an integer from this list (1-5, 0).", end="\n")
-        else:
-            break
-    if user_input == 1:
-        print("1")
-        return None
-        # arvix_scripts.pull_requests()
-    elif user_input == 2:
-        print("2")
-        return None
-    elif user_input == 3:
-        print("3")
-        return None
-    elif user_input == 4:
-        print("4")
-        return None
-    elif user_input == 5:
-        print("5")
-        return None
-    elif user_input == 0:
-        print("0")
-        return None
+        while True:
+            print("========")
+            print("Menu")
+            print("========")
+            print("1. Update database by search term from arxiv")
+            print("2. Visualizations by search term")
+            print("3. Summary of database")
+            print("4. Clean dupes from database")
+            print("e. Exit program")
+            print("w. Wipe database")
+            user_input = input("Pick your option (1-4, e, w).")
+            if user_input not in {"w","1","2","3","4","e"}:
+                print("Invalid answer. Please input an integer from this list (1-4) or letter from this list (e, w).", end="\n")
+            else:
+                break
 
+        if user_input == "1":
+            print("--")
+            print("Please input the search query or queries (separated by commas).")
+            print("Eg. 'radiation, plasmonics, metamaterials'")
+            user_queries = input("Search queries?")
+            
+            print("--")
+            print("Please input the additional search parameters (separated by commas).")
+            print("The format is [starting index, number of papers, report data changes (y,n), remove duplicates (y,n)]")
+            print("Eg. '5, 10, n, y'")
+            print("This will save the papers starting at the 5th paper and ending on the 14th paper (for total of 10 papers saved), won't show the data changes to the database, and will exclude saving duplicates.")
+            user_params = input("Query parameters?")
+
+            queries = [i.strip() for i in user_queries.split(",")]
+            start, max_results, verbose, remove_dupes = [i.strip() for i in user_params.split(",")]
+            arxiv.pull_requests(queries=queries, start=start, max_results=max_results, verbose=verbose, remove_dupes=remove_dupes)
+
+            print("Completed!")
+        elif user_input == "2":
+            print("--")
+            print("Please input the search query or queries (separated by commas).")
+            print("Eg. 'radiation, plasmonics, metamaterials'")
+            user_queries = input("Search queries?")
+
+            print("--")
+            print("Please input source restriction, if any. Currently, only one source can be queried at a time. If there are no restrictions, hit ENTER key again.")
+            print("Eg. 'arxiv'")
+            print("Eg. ''")
+            print("The first example will only analyze papers pulled from arxiv. The second example (which is the empty string) means you have no restrictions and the analysis will be performed on the entire database.")
+            user_params = input("Query parameters?")
+            if user_params == "":
+                user_params = "ALL"
+
+            queries = [i.strip() for i in user_queries.split(",")]
+            for query in queries:
+                current_session.graph_freq(query, user_params)
+            print("Completed!")
+        elif user_input == "3":
+            helper.db_summary()
+            print("Completed!")
+        elif user_input == "4":
+            helper.remove_dupes()
+            print("Completed!")
+        elif user_input == "e":
+            is_exiting = True
+            break
+        elif user_input == "w":
+            helper.reset_papers_db()
+            print("Completed!")
+    print("Thank you for using this program!")
+    return None
 
 if __name__ == "__main__":
     main()
